45098aec56d9821131bf88381d0dbbace09c1851
Kane Cleveland <kmclevel@silk25.uvm.edu>
2024-06-27 12:59:35 -0400

    got rid of duplicate code that i had in both scripts and dashboard.js

--- cs2450/js/scripts.js
+++ cs2450/js/scripts.js
@@ -29,67 +29,6 @@ document.addEventListener('DOMContentLoaded', async function() {
 
 
 
-function loadDashboard(content) {
-	if (content) {
-		// Init session variables as null until we can confirm they exist
-		let logged_in = null;
-		let username = null;
-		let user_id = null;
-		// Check to see if logged_in is even present in the sess_map
-		if ('logged_in' in sess_map) {
-			logged_in = sess_map.logged_in;
-		}
-		// if it is present, and it is true...
-		if (logged_in) {
-			username = sess_map.username;
-			user_id = sess_map.user_id;
-			content.innerHTML = `
-				<div class="dashboard">
-                    <div class="header">
-                        <h2>Welcome back, ${username}</h2>
-                    </div>
-                    <div class="synopsis">
-                        <p>Overview of your financial plan</p>
-                    </div>
-                    <div class="graphic" id="dashboardGraphic">
-                        <p>*insert relevant graphic*</p>
-                    </div>
-                </div>
-            `;	
-
-		} else {
-			content.innerHTML = `
-				<div class="dashboard">
-					<div class="header">
-						<h2>Dashboard</h2>
-					</div>
-					<div class="synopsis">
-						<p>Synopsis of the purpose and utility of the financial planner</p>
-					</div>
-					<div class="graphic" id="dashboardGraphic">
-						<p>*insert relevant graphic*</p>
-					</div>
-				</div>
-			`;
-		}
-        addEventListeners(logged_in); // Pass logged_in to the function
-    }
-}
-
-function addEventListeners(logged_in) {
-
-	const sitemap_button = document.getElementById('siteMapButton');
-	if (sitemap_button) {
-		console.log("sitemap button exists");
-		
-		sitemap_button.addEventListener('click', function() {
-			window.location.href = 'design/sitemap.php';
-		});
-	}
-				    
-	// If the page we are on is the dashboard, add event listeners to buttons in the dashboard
-}
-
 window.addEventListener('scroll', function() {
     var loginContainer = document.querySelector('.login-container');
     var signupContainer = document.querySelector('.signup-container');
