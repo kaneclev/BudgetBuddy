0dd8db4e3862540f84943f12667542dcf342b6ad
Kane Cleveland <kmclevel@silk25.uvm.edu>
2024-06-28 11:07:19 -0400

    starting the final step; adding some visualization stuff for the financial info the user has stored in the database.

--- cs2450/index.php
+++ cs2450/index.php
@@ -4,12 +4,17 @@ include('includes/top.php');
 
 <main>
     <div id="dashboard" root_path="/cs2450/">
-		<script>
-		<!-- Content will be loaded via my scripts.js function. -->
-		</script>
+        <script>
+        <!-- Content will be loaded via my scripts.js function. -->
+        </script>
     </div>
 </main>
 
+<!-- Add Chart.js library -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="./js/dashboard.js"></script>
+
 <?php
 include('includes/footer.php');
+?>
+
