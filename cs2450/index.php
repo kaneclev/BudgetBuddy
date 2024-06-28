6638a368f776eb4ae2f6313a0e4d4cf233120e19
Kane Cleveland <kmclevel@silk25.uvm.edu>
2024-06-28 11:19:02 -0400

    made space in index.php, source a chart module for javascritp that i can use to build my pie charts

--- cs2450/index.php
+++ cs2450/index.php
@@ -4,9 +4,25 @@ include('includes/top.php');
 
 <main>
     <div id="dashboard" root_path="/cs2450/">
-        <script>
-        <!-- Content will be loaded via my scripts.js function. -->
-        </script>
+        <div id="dashboard__header">
+            <h2 id="welcome__message">Welcome back, ${username}</h2>
+        </div>
+        <div id="dashboard__synopsis">
+            <p>Overview of your financial plan</p>
+        </div>
+        <div id="dashboard__graphic">
+            <img src="/cs2450/design/dash_banner.png" class="dashboard__banner" alt="Dashboard Banner">
+        </div>
+        <div id="charts-container">
+            <div class="chart">
+                <h3>Expenses by Category</h3>
+                <canvas id="expenseChart" width="200" height="200"></canvas>
+            </div>
+            <div class="chart">
+                <h3>Income by Category</h3>
+                <canvas id="incomeChart" width="200" height="200"></canvas>
+            </div>
+        </div>
     </div>
 </main>
 
