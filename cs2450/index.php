81d93deed882bbe4c77e1be3ec9d3dd81b146747
Kane Cleveland <kmclevel@silk25.uvm.edu>
2024-06-28 11:40:59 -0400

    created html structure to include charts (defined the container and divs), and also created a section below for additional info where I can get some words in.

--- cs2450/index.php
+++ cs2450/index.php
@@ -4,10 +4,36 @@ include('includes/top.php');
 
 <main>
     <div id="dashboard" root_path="/cs2450/">
-        <script>
-        <!-- Content will be loaded via my scripts.js function. -->
-        </script>
+        <div id="dashboard__header">
+            <h2 id="welcome__message">Welcome back, ${username}</h2>
+        </div>
+        <div id="dashboard__synopsis">
+            <p>Overview of your financial plan</p>
+        </div>
+        <div id="dashboard__graphic">
+            <img src="/cs2450/design/dash_banner.png" class="dashboard__banner" alt="Dashboard Banner">
+        </div>
+        <div id="charts-container">
+            <div class="chart">
+                <h3>Expenses by Category</h3>
+                <canvas id="expenseChart" width="50" height="50"></canvas>
+            </div>
+            <div class="chart">
+                <h3>Income by Category</h3>
+                <canvas id="incomeChart" width="50" height="50"></canvas>
+            </div>
+	  
+	
+		</div>
     </div>
+	
+
+	<div id="additional-content">
+            <h3>Additional Information</h3>
+            <p></p>
+     </div>
+
+
 </main>
 
 <!-- Add Chart.js library -->
